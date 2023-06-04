39b66163ca4db79fa39d02bea54aa2a767ee228e
Tony Boetto <dev@tonyboetto.net>
2023-06-04 14:24:46 -0400

    basic scripts for bar

--- config.c
+++ config.c
@@ -4,16 +4,21 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
+    /*{"sb-mail",    600,  1 },*/
+    {"sb-tasks",    10, 26 },
+    {"sb-music",   0,    11 },
+    {"sb-pacpackages",    0,  8 },
+    {"sb-torrent",    20, 7 },
     {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"sb-memory",  10,   14 },
+    {"sb-cpu",    10,   18 },
+    {"sb-gpu",    10,   28 },
+    {"sb-volume",  0,    10 },
+    /*{"sb-battery", 5,    3 },*/
+    {"sb-forecast",    18000,    5 },
+    {"sb-moonphase",    18000,    5 },
+    {"sb-clock",    60,    1 },
+    {"sb-internet",    5,    4 },
 };
 
 const unsigned short blockCount = LEN(blocks);
